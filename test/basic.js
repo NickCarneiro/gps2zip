36faf0bb0d0f5feeefbd2c3957a985b2f6cc488f
Nick Carneiro <nickc@indeed.com>
2014-11-23 12:35:26 -0800

    added unit test and benchmark

---
+++ test/basic.js
@@ -0,0 +1,29 @@
+var test = require('tape');
+
+var g = require('../');
+
+test('spot check results for a handful of zip codes', function(t) {
+    var austin = g.gps2zip(30.2859283, -97.7461031);
+    t.equal(austin['zip code'], '78705');
+
+    var sanFrancisco = g.gps2zip(37.7833, -122.4167);
+    t.equal(sanFrancisco['zip code'], '94102');
+
+    var chicago = g.gps2zip(41.8369, -87.6847);
+    t.equal(chicago['zip code'], '60608');
+
+    var boston = g.gps2zip(42.3581, -71.0636);
+    t.equal(boston['zip code'], '02108');
+
+    var seattle = g.gps2zip(47.6097, -122.3331);
+    t.equal(seattle['zip code'], '98101');
+
+    var marfa = g.gps2zip(30.3119, -104.0247);
+    t.equal(marfa['zip code'], '79843');
+
+    var roundRock = g.gps2zip(30.5086, -97.6789);
+    t.equal(roundRock['zip code'], '78664');
+
+    t.end();
+});
+
---
+++ test/benchmark.js
@@ -0,0 +1,21 @@
+var gps = require('../');
+
+// adapted from yildirim's snippet
+// https://github.com/NickCarneiro/gps2zip/issues/3
+
+var latitude = 37.4292;
+var longitude = -122.1381;
+var dlon = 1.0;
+var nn = 1000;
+
+var hrStart = process.hrtime();
+
+for (var i=0; i < nn; i++){
+    longitude -= dlon/nn;
+    var response = gps.gps2zip(latitude, longitude);
+}
+
+var hrEnd = process.hrtime(hrStart);
+
+// seeing 3.5 - 4.5 seconds on 2011 MBP
+console.info("Execution time (hr): %ds %dms", hrEnd[0], hrEnd[1]/1000000);
