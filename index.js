241ddfb74b1b8babe0be383049f3c6859b83169f
Nick Carneiro <nickc@indeed.com>
2014-11-28 10:56:47 -0600

    switch from kdtree library to kdt because it will work with browserify

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //load zip csv into memory into a format we can easily iterate over
 //executed when gps2zip is required
 var fs = require('fs');
-var kd = require('kdtree');
+var kdt = require('kdt');
 var zips = [];
 try {
 	// zip code data is sorted by ascending latitude
@@ -10,20 +10,19 @@ try {
 
 	//zips is a global defined up top
 	zips = JSON.parse(data);
-
-	var tree = new kd.KDTree(2);
-	for (var i = 0; i < zips.length; i++){
-		tree.insert(zips[i].latitude, zips[i].longitude, i);
-	}
+	var distance = function(a, b){
+		return Math.pow(a.latitude - b.latitude, 2) +  Math.pow(a.longitude - b.longitude, 2);
+	};
+	var tree = kdt.createKdTree(zips, distance, ['latitude', 'longitude']);
 } catch (err) {
 	console.error("There was an error opening the zip code file:");
 	console.log(err);
 }
 
-exports.gps2zip = function(lat, lon){
-
-	var result = tree.nearest(lat, lon);
-	var nearestIndex = result[2];
-	return zips[nearestIndex];
-
+exports.gps2zip = function(latitude, longitude){
+	// we've already constructed the kd-tree. Just find the nearest location
+	var results =  tree.nearest({'latitude': latitude, 'longitude': longitude}, 1);
+	var firstResult = results[0];
+	var zipCode = firstResult[0];
+	return zipCode;
 };
