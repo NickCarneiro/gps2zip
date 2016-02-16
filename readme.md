c5e62c156d83f208833ad6ff9d0e9f1950ed5257
Nick Carneiro <nickc@trillworks.com>
2016-02-16 13:27:51 -0800

    fixed unit tests, version bump for new zip codes

--- readme.md
+++ readme.md
@@ -1,23 +1,23 @@
-	
+
 # Gps2zip
 
 Convert GPS latitude longitude pairs to a US zip code.
 
 # Quick Start
 
-	npm install gps2zip
+  npm install gps2zip
 
 # Usage
 
-	var gps = require('gps2zip');
-	var latitude = 30.2669;
-	var longitude = 97.7428;
-	gps.gps2zip(latitude, longitude);
-	// returns 78701
+  var gps = require('gps2zip');
+  var latitude = 30.2669;
+  var longitude = 97.7428;
+  gps.gps2zip(latitude, longitude);
+  // returns 78701
 
 # Unit tests
 
-	node tests/basic.js
+  node test/basic.js
 
 # Accuracy
 The output is approximate. The source data is just a list of coordinates, not actual boundaries. The algorithm just finds the minimum euclidean distance between the point in question and the known points. No error checking for points outside the United States.
--- test/basic.js
+++ test/basic.js
@@ -4,25 +4,25 @@ var g = require('../');
 
 test('spot check results for a handful of zip codes', function(t) {
     var austin = g.gps2zip(30.2859283, -97.7461031);
-    t.equal(austin['zip_code'], '78705');
+    t.equal(austin['zip_code'], 78705);
 
     var sanFrancisco = g.gps2zip(37.7833, -122.4167);
-    t.equal(sanFrancisco['zip_code'], '94102');
+    t.equal(sanFrancisco['zip_code'], 94102);
 
     var chicago = g.gps2zip(41.8369, -87.6847);
-    t.equal(chicago['zip_code'], '60608');
+    t.equal(chicago['zip_code'], 60608);
 
     var boston = g.gps2zip(42.3581, -71.0636);
-    t.equal(boston['zip_code'], '02108');
+    t.equal(boston['zip_code'], 02108);
 
     var seattle = g.gps2zip(47.6097, -122.3331);
-    t.equal(seattle['zip_code'], '98101');
+    t.equal(seattle['zip_code'], 98101);
 
     var marfa = g.gps2zip(30.3119, -104.0247);
-    t.equal(marfa['zip_code'], '79843');
+    t.equal(marfa['zip_code'], 79843);
 
     var roundRock = g.gps2zip(30.5086, -97.6789);
-    t.equal(roundRock['zip_code'], '78664');
+    t.equal(roundRock['zip_code'], 78664);
 
     t.end();
 });
